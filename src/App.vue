90c6c391ea4f1dd655a1032d0471e9864f186c9d
Filipp Zhuravlev <shiningfinger@list.ru>
2018-08-26 14:07:58 +0300

    Gogo.

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<style src='@/assets/sass/main.sass' lang='sass'></style>
+<style src='@/assets/sass/fonts.sass' lang='sass'></style>
+
+<template>
+  <div id="app">
+    <header>
+      Header
+    </header>
+    <main>
+      <router-view/>
+    </main>
+    <footer>
+      Footer
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+<style>
+body {
+  margin: 0;
+}
+
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+} */
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495e;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: 0.02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="out">
+        
+        
+    Main
+        
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  delimiters: [],
+  components: {},
+  filters: {},
+  props: {},
+  data: () => ({}),
+  computed: {},
+  beforeMount() {},
+  mounted() {},
+  beforeCreate() {},
+  created() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {},
+  fetch() {},
+  beforeUpdated() {}
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
